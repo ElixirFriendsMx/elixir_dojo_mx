def764ba959885b6cba1b765628d08a27794c2b8
sohjiro <felipe@makingdevs.com>
2015-09-03 22:39:50 -0500

    module refactor

--- session_1/fizzbuzz/d5lment_sohjiro/homework/book_store.exs
+++ session_1/fizzbuzz/d5lment_sohjiro/homework/book_store.exs
@@ -1,27 +1,23 @@
 defmodule Bookstore do
 
   def calculate_total(taxes, orders = [_head | _tail]) do
-    calculate_total_amount([], Enum.into(taxes, %{}), orders)
+    orders
+    |> Enum.map(&(process_data(&1, taxes)))
   end
 
-  defp calculate_total_amount(data, %{NC: tax} = taxes , [ head = [id: _id, ship_to: :NC, net_amount: amount] | tail]) do
-    [[head | [total: amount * (tax + 1)]] | data]
-    |> calculate_total_amount(taxes, tail)
+  def process_data(data, taxes) do
+    taxes
+    |> Enum.into(%{})
+    |> get_tax_for(data[:ship_to])
+    |> Enum.map(&calculate_tax(&1, data))
   end
 
-  defp calculate_total_amount(data, %{TX: tax} = taxes, [ head = [id: _id, ship_to: :TX, net_amount: amount] | tail]) do
-    [[head | [total: amount * (tax + 1)]] | data]
-    |> calculate_total_amount(taxes, tail)
-  end
+  defp get_tax_for(%{NC: tax}, :NC), do: %{tax: tax + 1}
+  defp get_tax_for(%{TX: tax}, :TX), do: %{tax: tax + 1}
+  defp get_tax_for(_, _), do: %{tax: 1}
 
-  defp calculate_total_amount(data, taxes = %{}, [ head = [id: _id, ship_to: _ship_to, net_amount: amount] | tail]) do
-    [[head | [total: amount]] | data]
-    |> calculate_total_amount(taxes, tail)
-  end
-
-  defp calculate_total_amount(data, _, []) do
-    data
-    |> Enum.reverse
+  defp calculate_tax({_, tax}, data) do
+    data ++ [total: data[:net_amount] * tax]
   end
 
 end

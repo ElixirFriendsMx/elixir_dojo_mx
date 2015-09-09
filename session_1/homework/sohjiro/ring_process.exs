e24ce0e0b377319760e21cb52bf3b09b25602309
sohjiro <felipe@makingdevs.com>
2015-09-09 00:10:21 -0500

    new approach

--- session_1/homework/sohjiro/ring_process.exs
+++ session_1/homework/sohjiro/ring_process.exs
@@ -1,42 +1,20 @@
 defmodule RingProcess do
-
   def start do
     spawn(fn ->
       Process.register(self, :server_a)
-      loop_a()
-    end)
-
-    spawn(fn ->
-      Process.register(self, :server_b)
-      loop_b()
+      loop()
     end)
   end
 
-  defp loop_a do
+
+  defp loop do
     receive do
-      {:kill} ->
-        IO.puts "killing a"
       {_message, _times, 0} ->
-        IO.puts "Sending message to kill b"
-        send(:server_b, {:kill})
+        IO.puts "killing server"
       {message, times, remaining} ->
         IO.puts "ping: :message : #{message} times : #{times}, remaining : #{remaining}"
-        send(:server_b, {message, times, remaining - 1})
-        loop_a
-    end
-  end
-
-  defp loop_b do
-    receive do
-      {:kill} ->
-        IO.puts "killing b"
-      {_message, _times, 0} ->
-        IO.puts "Sending message to kill a"
-        send(:server_a, {:kill})
-      {message, times, remaining} ->
-        IO.puts "pong: :message : #{message} times : #{times}, remaining : #{remaining}"
         send(:server_a, {message, times, remaining - 1})
-        loop_b
+        loop()
     end
   end
 

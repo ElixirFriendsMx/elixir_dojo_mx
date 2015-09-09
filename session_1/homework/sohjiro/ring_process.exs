aff0fb52b5c916ddd36d8629d6db5bbca71f7f4c
sohjiro <felipe@makingdevs.com>
2015-09-08 23:36:31 -0500

    sending message across 2 nodes

--- session_1/homework/sohjiro/ring_process.exs
+++ session_1/homework/sohjiro/ring_process.exs
@@ -14,20 +14,25 @@ defmodule RingProcess do
 
   defp loop_a do
     receive do
+      {:kill} ->
+        IO.puts "killing a"
       {_message, _times, 0} ->
-        IO.puts "Process terminated for :server_a"
-        send(:server_b, {0, 0, 0})
+        IO.puts "Sending message to kill b"
+        send(:server_b, {:kill})
       {message, times, remaining} ->
         IO.puts "ping: :message : #{message} times : #{times}, remaining : #{remaining}"
-        send(:server_b, {message, times, remaining})
+        send(:server_b, {message, times, remaining - 1})
         loop_a
     end
   end
 
   defp loop_b do
     receive do
+      {:kill} ->
+        IO.puts "killing b"
       {_message, _times, 0} ->
-        IO.puts "Process terminated for :server_b"
+        IO.puts "Sending message to kill a"
+        send(:server_a, {:kill})
       {message, times, remaining} ->
         IO.puts "pong: :message : #{message} times : #{times}, remaining : #{remaining}"
         send(:server_a, {message, times, remaining - 1})

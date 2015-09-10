db3471578f32180ed9b0e79d3cb1ad7b873753c2
sohjiro <felipe@makingdevs.com>
2015-09-10 01:01:15 -0500

    sending a message around the ring

--- session_1/homework/sohjiro/ring_process.exs
+++ session_1/homework/sohjiro/ring_process.exs
@@ -9,8 +9,13 @@ defmodule RingProcess do
 
   defp loop do
     receive do
+      {_, _, 0} ->
+        IO.puts "Ending ring process"
       {[h | t], message, times} ->
         IO.puts "#{inspect h}, #{inspect t}, #{message} : #{times}"
+        send(h, message)
+        :timer.sleep(1000)
+        send(:ring, {t ++ [h], message, times - 1})
         loop()
     end
   end
@@ -22,13 +27,11 @@ defmodule RingProcess do
 end
 
 defmodule NodeProcess do
-
   def loop do
     receive do
       message ->
-        IO.puts "message incoming : #{message}"
+        IO.puts "#{inspect self} : #{message}"
         loop()
     end
   end
-
 end

a38b3330ac9d79ebdae25785ae3bd43af7aa33c1
sohjiro <felipe@makingdevs.com>
2015-09-09 20:02:07 -0500

    separating modules

--- session_1/homework/sohjiro/ring_process.exs
+++ session_1/homework/sohjiro/ring_process.exs
@@ -1,25 +1,21 @@
 defmodule RingProcess do
   def start do
-    spawn(fn ->
-      Process.register(self, :server_a)
-      loop()
-    end)
+    spawn_monitor(NodeProcess, :loop, [])
   end
 
 
   defp loop do
+  end
+
+end
+
+defmodule NodeProcess do
+
+  def loop do
     receive do
-      {_message, _times, 0} ->
-        IO.puts "killing server"
-      {message, times, remaining} ->
-        IO.puts "ping: :message : #{message} times : #{times}, remaining : #{remaining}"
-        send(:server_a, {message, times, remaining - 1})
+      message ->
+        IO.puts "message incoming : #{message}"
         loop()
     end
   end
-
-  def send_message(message, times) do
-    send(:server_a, {message, times, times})
-  end
-
 end

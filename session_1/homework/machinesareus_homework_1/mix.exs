ae2925e4b2c7661cc39360975921c194be8cd589
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-09-10 03:48:34 -0500

    renamed project and folder

--- session_1/homework/machinesareus_homework_1/mix.exs
+++ session_1/homework/machinesareus_homework_1/mix.exs
@@ -2,7 +2,7 @@ defmodule ProcessRing.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :homework1,
+    [app: :machinesareus_homework_1,
      version: "0.0.1",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,

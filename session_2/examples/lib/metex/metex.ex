ef0c41cfb998d774d0a0c9a93d92472889faca26
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-09-10 03:24:11 -0500

    fixed bug

--- session_2/examples/lib/metex/metex.ex
+++ session_2/examples/lib/metex/metex.ex
@@ -1,8 +1,8 @@
 defmodule Metex do
 
   def temperatures_of(cities) do
-    coordinator_pid = spawn(Metex.Coordinator, :loop, [[], length(cities)])
-    cities |> Enum.each fn city ->
+    coordinator_pid = spawn Metex.Coordinator, :loop, [[], length(cities)]
+    cities |> Enum.each fn(city) ->
       worker_pid = spawn(Metex.Worker, :loop, [])
       send worker_pid, {coordinator_pid, city}
     end 

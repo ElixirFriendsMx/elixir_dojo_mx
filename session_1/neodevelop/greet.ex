e67dc3b589c30dae2ed8ff44fdd9c502fa34c2ae
neodevelop <neodevelop@gmail.com>
2015-09-10 20:22:17 -0500

    Creation of many process, identifyng the process

--- session_1/neodevelop/greet.ex
+++ session_1/neodevelop/greet.ex
@@ -1,13 +1,13 @@
 defmodule ConsoleGreeter do
-  def greet do
+  def greet(id) when is_integer(id) do
     receive do
       :greet ->
-        IO.puts "Hello world"
-        greet
+        IO.puts "Hello world: from #{id}"
+        greet id
       :bye ->
-        IO.puts "bye"
+        IO.puts "bye pid #{id}"
       _ ->
-        greet
+        greet id
     end
   end
 end

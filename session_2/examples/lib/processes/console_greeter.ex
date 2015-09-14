597ffbca1432b5ccd0e4bf83b4e813d9c0719168
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-09-14 00:19:34 -0500

    updated greeter examples

--- session_2/examples/lib/processes/console_greeter.ex
+++ session_2/examples/lib/processes/console_greeter.ex
@@ -3,12 +3,7 @@ defmodule ConsoleGreeter do
   def greet do
     receive do
       :greet -> 
-        IO.puts "hello world! from #{inspect self}"
-        greet 
-      :bye -> 
-        IO.puts "bye! from #{inspect self}"
-      _ ->
-        greet
+        IO.puts "hello world"
     end
   end
 

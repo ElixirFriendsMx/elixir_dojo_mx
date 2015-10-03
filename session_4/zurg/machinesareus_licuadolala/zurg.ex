8116a2929cd17498e5956b7a168fda26f4b0984f
Agustín Ramos Fonseca <zentimental.software@gmail.com>
2015-10-03 15:53:59 -0500

    pimping zurg

--- session_4/zurg/machinesareus_licuadolala/zurg.ex
+++ session_4/zurg/machinesareus_licuadolala/zurg.ex
@@ -34,35 +34,31 @@ defmodule Zurg do
 
   def next_steps(seq = {dir, _time, left, right, _steps}) do
     case dir do
-      :right -> 
-         options(left)
-           |> map(&cross_right(seq, &1))
-      :left ->
-         right
-           |> map(&cross_left(seq, &1))
-    end
+      :right -> pairs left 
+      :left -> right
+    end |> map(&step(seq, &1))
   end
 
-  def cross_right({dir, time, left, right, steps}, {t1,t2}) do
+  def step({:right, time, left, right, steps}, {t1,t2}) do
+    n_time  = time + max(@toys[t1], @toys[t2])
     n_left  = left -- [t1, t2]
     n_right = right ++ [t1, t2]
-    n_time  = time + max(@toys[t1], @toys[t2])
-    n_steps = steps ++ [{dir,t1,t2}]
+    n_steps = steps ++ [{:right, t1, t2}]
     {:left, n_time, n_left, n_right, n_steps}
   end
 
-  def cross_left({dir, time, left, right, steps}, toy) do
+  def step({:left, time, left, right, steps}, toy) do
+    n_time  = time + @toys[toy]
     n_left  = left ++ [toy]
     n_right = right -- [toy]
-    n_time  = time + @toys[toy]
-    n_steps = steps ++ [{dir, toy}]
+    n_steps = steps ++ [{:left, toy}]
     {:right, n_time, n_left, n_right, n_steps}
   end
 
-  def options(toys) do
-    opts = for t1 <- toys, t2 <- toys, t1 != t2, do: [t1, t2]
-    opts |> map(&sort/1)
-         |> uniq
-         |> map(&List.to_tuple/1)
+  def pairs(toys) do
+    all = for t1 <- toys, t2 <- toys, t1 != t2, do: [t1, t2]
+    all |> map(&sort/1)
+        |> uniq
+        |> map(&List.to_tuple/1)
   end
 end
